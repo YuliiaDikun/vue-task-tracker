d64973877cb45dbc4ccd3d199bbc64c6b3751fe7
Yuliia Dikun <dikun.yulia@gmail.com>
2023-02-08 14:01:03 +0200

    create: button comp and tasks data

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,57 @@
 <template>
-  <h1>Hello World</h1>
+  <div class="container">
+    <ThisHeader title="Task Tracker" />
+  </div>
 </template>
 
 <script>
+import ThisHeader from "./components/Header";
 export default {
   name: "App",
-  components: {},
+  components: {
+    ThisHeader,
+  },
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  created() {
+this.tasks = [
+   [
+    {
+      id: "1",
+      text: "Doctors Appointment",
+      day: "March 5th at 2:30pm",
+      reminder: true
+    },
+    {
+      id: "2",
+      text: "Meeting with boss",
+      day: "March 6th at 1:30pm",
+      reminder: true
+    },
+    {
+      id: "3",
+      text: "Food shopping",
+      day: "March 7th at 2:00pm",
+      reminder: false
+    }
+  ]
+]
+  }
 };
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
 * {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
 }
 body {
-  font-family: 'Poppins', sans-serif;
+  font-family: "Poppins", sans-serif;
 }
 .container {
   max-width: 500px;
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <ThisButton text='Add task' color='green'/>
+    </header>
+</template>
+
+<script>
+
+import ThisButton from './Button'
+export default {
+    name: 'ThisHeader',
+    props: {
+        title: String
+    },
+    components: {
+        ThisButton
+    }
+}
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
