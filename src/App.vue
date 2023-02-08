0fd1713b483d4e1d0f2a7fcdfa13e6c9ddd404f1
Yuliia Dikun <dikun.yulia@gmail.com>
2023-02-08 14:16:22 +0200

    create: tasks list comp

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,47 @@
 <template>
   <div class="container">
     <ThisHeader title="Task Tracker" />
+    <MyTasks :tasks="tasks" />
   </div>
 </template>
 
 <script>
 import ThisHeader from "./components/Header";
+import MyTasks from "./components/Tasks";
+
 export default {
   name: "App",
   components: {
     ThisHeader,
+    MyTasks,
   },
   data() {
     return {
-      tasks: []
-    }
+      tasks: [],
+    };
   },
   created() {
-this.tasks = [
-   [
-    {
-      id: "1",
-      text: "Doctors Appointment",
-      day: "March 5th at 2:30pm",
-      reminder: true
-    },
-    {
-      id: "2",
-      text: "Meeting with boss",
-      day: "March 6th at 1:30pm",
-      reminder: true
-    },
-    {
-      id: "3",
-      text: "Food shopping",
-      day: "March 7th at 2:00pm",
-      reminder: false
-    }
-  ]
-]
-  }
+    this.tasks = [
+      {
+        id: "1",
+        text: "Doctors Appointment",
+        day: "March 5th at 2:30pm",
+        reminder: true,
+      },
+      {
+        id: "2",
+        text: "Meeting with boss",
+        day: "March 6th at 1:30pm",
+        reminder: true,
+      },
+      {
+        id: "3",
+        text: "Food shopping",
+        day: "March 7th at 2:00pm",
+        reminder: false,
+      },
+    ];
+  },
 };
 </script>
 
